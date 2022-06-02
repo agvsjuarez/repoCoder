dffe091529d627d4095d52fe3a1d04fa9fc05d9d
Agustín Juárez <agustinjuarez1365@gmail.com>
2022-06-01 23:40:32 -0300

    Primera entrega curso JS

---
+++ js/script2.js
@@ -0,0 +1,7 @@
+// PEDIR UN NUMERO INGRESADO POR TECLADO E IMPRIMIR LA PALABRA "CABLE" LA CANTIDAD DE VECES EQUIVALENTE AL NUMERO PREVIAMENTE INGRESADO
+
+var numero = parseInt(prompt("Ingrese el numero de veces que desea imprimir la palabra: "));
+
+for(let i=0; i<numero; i++){
+	alert("CABLE");
+}
