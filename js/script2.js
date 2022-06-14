e4d1d690844d979ab5254a125ed4a1fd0e825a0d
Agustín Juárez <agustinjuarez1365@gmail.com>
2022-06-14 20:06:40 -0300

    arrays

--- js/script2.js
+++ js/script2.js
@@ -11,6 +11,7 @@ for(let i=0; i<numero; i++){
 //Aqui se solicita el ingreso del nombre del cliente y la cantidad a comprar de cable. 
 //Luego se suma la cantidad de cable total a fabricar para la empresa y se lo muestra por pantalla.
 
+/*
 function ingresarCantidad(){
 	
 	let cantidad = parseInt(prompt("Ingrese la cantidad de metros de cable que desea: "));
@@ -34,3 +35,25 @@ while(nombre != "ESC"){
 }
 
 alert("La cantidad total de cable para fabricar es de: " + cantidadTotal);
+*/
+
+//Aqui se solicita el ingreso de los nombres de los empleados que trabajan en la fábrica
+//Y luego imprime el listado de ellos y la cantidad para obtener un control
+
+const arrayNombres = [];
+let cantidadEmpleados = 0;
+
+let nombre = prompt("Ingresa tu nombre o 1 para salir: ");
+
+while(nombre != "1"){
+
+	arrayNombres.push(nombre);
+
+	nombre = prompt("Ingresa tu nombre o 1 para salir: ");
+}
+
+cantidadEmpleados = arrayNombres.length - 1;
+console.log("La cantidad de empleados que ingresaron fue: " + cantidadEmpleados);
+console.log("Los empleados que ingresaron fueron: " + arrayNombres.join(';'))
+
+
