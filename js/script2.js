3e84f9462a904a95f08418faa50ad5041f22573f
Agustín Juárez <agustinjuarez1365@gmail.com>
2022-06-16 13:37:13 -0300

    Primera entrega proyecto final

--- js/script2.js
+++ js/script2.js
@@ -40,6 +40,7 @@ alert("La cantidad total de cable para fabricar es de: " + cantidadTotal);
 //Aqui se solicita el ingreso de los nombres de los empleados que trabajan en la fábrica
 //Y luego imprime el listado de ellos y la cantidad para obtener un control
 
+/*
 const arrayNombres = [];
 let cantidadEmpleados = 0;
 
@@ -55,5 +56,73 @@ while(nombre != "1"){
 cantidadEmpleados = arrayNombres.length - 1;
 console.log("La cantidad de empleados que ingresaron fue: " + cantidadEmpleados);
 console.log("Los empleados que ingresaron fueron: " + arrayNombres.join(';'))
+*/
+
+//DESAFIO PRE-ENTREGA N1 PROYECTO FINAL
+//Se solicita el ingreso de clientes que compren articulos de la empresa de cables, con sus respectivos datos (NOMBRE, EDAD, TELOFONO Y DIRECCION).
+//Luego se imprimen estos datos por pantalla y se realiza un listado de ellos con todos sus datos por separado.
+//Tambien se le asigna un numero de cliente a cada quien que ingrese, comenzando del 0 en adelante.
 
 
+function Persona(nombre, edad, tel, direc){
+
+	this.nombre = nombre;
+	this.edad = edad;
+	this.tel = tel;
+	this.direc = direc;
+
+} 
+
+let nombre = prompt("Ingresa tu nombre o ESC para salir: ");
+let numeroCliente = 0;
+const nombres = [];
+const edades = [];
+const telefonos = [];
+const direcciones = [];
+
+while(nombre != "ESC"){
+
+	console.log("Hola " + nombre.toUpperCase() + "! , usted es el cliente: " + numeroCliente);
+
+	nombres.push(nombre);
+
+	let edad = parseInt(prompt("Ingrese su edad: "));
+
+	edades.push(edad);
+
+	let tel = prompt("Ingresa tu telefono: ");
+
+	telefonos.push(tel);
+
+	let direc = prompt("Ingresa tu direccion: ");
+
+	direcciones.push(direc);
+
+	const personaVariable = new Persona(nombre, edad, tel, direc); 
+
+	for(const propiedad in personaVariable){
+		console.log(propiedad + ": " + personaVariable[propiedad]);
+	}
+
+	numeroCliente = numeroCliente + 1;
+
+	nombre = prompt("Ingresa tu nombre o ESC para salir: ");
+}
+
+console.log("La cantidad de clientes totales ingresados fue de: " + nombres.length);
+
+for(let index=0; index < nombres.length; index++){
+	console.log("Nombre del cliente " + index + " : " + nombres[index]);
+}
+
+for(let index=0; index < edades.length; index++){
+	console.log("Edad del cliente " + index + " : " + edades[index]);
+}
+
+for(let index=0; index < telefonos.length; index++){
+	console.log("Telefono del cliente " + index + " : " + telefonos[index]);
+}
+
+for(let index=0; index < direcciones.length; index++){
+	console.log("Direccion del cliente " + index + " : " + direcciones[index]);
+}
